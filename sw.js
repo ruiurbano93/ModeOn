58339b860e52d165779cd475fd67a6c0ffb024ef
ruiurbano93 <ruiurbano93@gmail.com>
2026-07-08 22:39:35 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'vitalsync-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const resClone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, resClone));
+      return res;
+    }).catch(() => caches.match('/index.html')))
+  );
+});
+
